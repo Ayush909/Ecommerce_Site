69e64e53cdb8ce4041ef9e3042a4bdcf0cbab977
Ayush Singh Kushwaha <50234003+Ayush909@users.noreply.github.com>
2022-06-06 01:31:52 +0530

    material ui intro

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,29 @@
 import './App.css';
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
+import Navbar from './components/Navbar/Navbar';
+import Header from './components/Header/Header';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+
+const theme = createTheme({
+  palette : {
+    primary : {
+      main : "#ff6666"
+    }
+  }
+})
 
 function App() {
   return (
-    <div className="App">
-      <h1>sdas</h1>
-    </div>
+    <ThemeProvider theme={theme}>
+      <Router>
+        <Navbar/>
+        <Header/>
+        <Routes>
+          <Route path=''/>
+        </Routes>
+        
+      </Router>
+    </ThemeProvider>
   );
 }
 
---
+++ frontend/src/components/Navbar/Navbar.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import Button from '@mui/material/Button';
+import {Typography} from '@mui/material'
+
+
+
+const Navbar = () => {
+  return (
+    <div>
+        <Typography 
+            variant='h2' 
+            color="primary" 
+            align='center' 
+            
+        >
+            Hello world
+        </Typography>
+    </div>
+  )
+}
+
+export default Navbar
